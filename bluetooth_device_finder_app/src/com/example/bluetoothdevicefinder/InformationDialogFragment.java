8b2ccb8d69a48e01f5d487f3ebacdbb90833622e
Stefan Tscheppe <tscheppe@student.tugraz.at>
2013-06-20 13:11:18 +0200

    [ST] Performance verbessert

--- bluetooth_device_finder_app/src/com/example/bluetoothdevicefinder/InformationDialogFragment.java
+++ bluetooth_device_finder_app/src/com/example/bluetoothdevicefinder/InformationDialogFragment.java
@@ -5,8 +5,6 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 
 import com.example.bluetoothdevicefinder.MainActivity;
 
